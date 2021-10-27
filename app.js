d7254045e050b4adc455ccc34f0e254d09b15bb0
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2021-10-27 22:21:26 +0100

    styles bug

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ dotenv.config();
 // express app
 const app = express();
 
-// connect to mongodb
-const dbURI = process.env.DBURI;
-const port = process.env.PORT || 800;
-mongoose
-  .connect(dbURI)
-  .then((result) => app.listen(port))
-  .catch((err) => console.log(err));
-
 // register view engine
 app.set("view engine", "ejs");
 
@@ -40,3 +32,11 @@ app.use("/blogs", blogRoutes);
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
+
+// connect to mongodb
+const dbURI = process.env.DBURI;
+const port = process.env.PORT || 8000;
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
